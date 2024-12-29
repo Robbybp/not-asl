dc0a082c784aed77a0aa1cdc8fe6d9d3028c8311
robbybp <robbybparker@gmail.com>
2024-12-29 13:11:12 -0500

    add tests for product

--- src/test-diff.c
+++ src/test-diff.c
@@ -20,6 +20,9 @@ int main(int narg, char ** argv){
   union NodeData xdata = {.var = &x};
   union NodeData ydata = {.var = &y};
   union NodeData zdata = {.var = &z};
+  struct Node xnode = {VAR_NODE, xdata};
+  struct Node ynode = {VAR_NODE, ydata};
+  struct Node znode = {VAR_NODE, zdata};
 
   struct Variable variables[3] = {x, y, z};
 
@@ -39,6 +42,37 @@ int main(int narg, char ** argv){
   deriv = differentiate_expression(expr2, nvar);
   to_string(buffer, bsize, expr2);
   printf("\nExpression: %s", buffer);
+  print_csrmatrix(deriv);
+  free_csrmatrix(deriv);
+
+  struct Node expr3;
+  expr3.type = OP_NODE;
+  struct Node expr3args[2] = {xnode, znode};
+  struct OperatorNode e3op = {
+    .op = PRODUCT,
+    .nargs = 2,
+    .args = expr3args,
+    .value = 0.0,
+    .adjoint = 0.0,
+  };
+  union NodeData e3data = {.expr = &e3op};
+  expr3.data = e3data;
+  to_string(buffer, bsize, expr3);
+  printf("\nExpression: %s", buffer);
+  deriv = differentiate_expression(expr3, nvar);
+  print_csrmatrix(deriv);
+  free_csrmatrix(deriv);
+
+  struct Node expr4; expr4.type = OP_NODE;
+  struct Node expr4args[2] = {expr3, znode};
+  struct OperatorNode e4op = {
+    PRODUCT, 2, expr4args, 0.0, 0.0
+  };
+  union NodeData e4data = {.expr = &e4op};
+  expr4.data = e4data;
+  to_string(buffer, bsize, expr4);
+  printf("\nExpression: %s", buffer);
+  deriv = differentiate_expression(expr4, nvar);
   print_csrmatrix(deriv);
   free_csrmatrix(deriv);
 
